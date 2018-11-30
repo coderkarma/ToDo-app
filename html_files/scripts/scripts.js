6d2e0b40ef982f2553851f87ca2c98e706f0f98b
Karma Drukpa <karmadrukpa11@gmail.com>
2018-11-30 18:40:04 +0000

    adding backend

--- html_files/scripts/scripts.js
+++ html_files/scripts/scripts.js
@@ -34,15 +34,27 @@ $(document).ready( () => {
       $.post("https://app-todo-coderkarma.c9users.io/newTask", newData, (response, error) => {
           
          $("#message").text("Task Added!");
+          $("#message").fadeOut(2000, () => {
+              $(this).text("");
+              $(this).fadeIn(0);
+          });
+         
+         
          $("#todo-list ul").empty();
           
           for (let i = 0; i < response.tasks.length; i++) {
             console.log(response.tasks[i]);
             
-            let html = '<li><input type="checkbox">' + response.tasks[i] + '<button>Edit</button><button>Delete</button></li>';
+            let html = '<li><input type="checkbox">' + response.tasks[i] + '<button>Edit</button><button class="delete-button">Delete</button></li>';
             
             $('#todo-list ul').append(html);
-        }
+            
+            }
+            
+            $(".delete-button").click(function (){
+                 console.log("Delete button pressed on no!!")
+              
+              }); 
           
       });
     });
--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 let fs = require("fs");
 let bodyParser = require("body-parser");
 let express = require("express");
+let crypto = require("crypto");
+let mongoose = require("mongoose");
+
+
+let dbUrl = "mongodb://coderkarma:123456789k@ds119394.mlab.com:19394/todo-fall2018";
+
+
+mongoose.connect(dbUrl, function(error){
+    if(error){
+        console.log("MongoDB connection failed");
+    } else {
+        console.log("MongoDB connectino successfull");
+    }
+});
+
+
+let Schema = mongoose.Schema;
+
+
 
 let app = express();
 
@@ -12,6 +31,29 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 http.listen(8080);
+
+function generateRandomString(length){
+    let hex = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
+    let string = "";
+    
+    for ( let i = 0; i < length; i++){
+        string += hex.charAt(Math.floor(Math.random() * hex.length))
+    }
+    return string;
+}
+
+
+let Task = Schema({
+    "text": String,
+    "status": String,
+    "dueDate": String,
+    "taskId": String
+});
+
+
+let TodoModel = mongoose.model("task", Task);
+
+
 
 console.log("Express server is now running on port 8080");
 
@@ -28,17 +70,42 @@ app.post("/fetchTasks", (request, response) => {
 
 
 app.post("/newTask", (request, response) => {
-    let recievedData = request.body;
-    let newTask = recievedData.task;
-     console.log(tasks);
-    tasks.push(newTask);
-
-    console.log(tasks);
+     let newTask = request.body;
+     
+     let newTaskForDB = {
+         "text": newTask.task,
+         "status": "new",
+         "dueDate": newTask.dueDate,
+         "taskId": crypto.createHash("md5").update(newTask.task).digest("hex") + generateRandomString(8)
+     }
     
-     let data = {
-        "tasks": tasks
-    };
+    let taskToSubmit = TodoModel(newTaskForDB);
+    taskToSubmit.save();
+    
+    let data = {
+        "task": "" // mdb request , all task items
+    }
     
     response.send(data);
     
+});
+
+app.post("/deleteTask", function (request, response) {
+    
+    let recievedData = request.body;
+    let taskToDelete = recievedData.task;
+    
+    let index = tasks.indexOf(taskToDelete);
+    
+    if (index > -1) {
+        tasks.splice(index, 1);
+    }
+    
+    console.log(tasks);
+    
+    let data = {
+        "tasks": tasks
+    }
+    
+    response.send(data);
 });
