d77450adc618d85aaf5684caad0b18ddebc10141
Karma Drukpa <karmadrukpa11@gmail.com>
2018-12-03 19:48:18 +0000

    update

--- html_files/scripts/scripts.js
+++ html_files/scripts/scripts.js
@@ -1,62 +1,97 @@
 /* global $ */
 
-$(document).ready( () => {
-    console.log("Ready");
+// Updates the web page task list, takes in an array of tasks.
+function updateTaskList(tasks) {
     
-    let data = {
-    };
-    // fetchTask is the handler for the server
-    $.post("https://app-todo-coderkarma.c9users.io/fetchTasks", data, (response) => {
+    // Empty out existing web page task list.
+    $("#todo-list ul").empty();
+    
+    // Loop through the array of tasks.
+    for (let i = 0; i < tasks.length; i++) {
         
-        for (let i = 0; i < response.tasks.length; i++) {
-            console.log(response.tasks[i]);
-            
-            let html = '<li><input type="checkbox">' + response.tasks[i] + '<button>Edit</button><button>Delete</button></li>';
-            
-            $('#todo-list ul').append(html);
-        }
-        
-    });
-    
-    $('#submitNewTask').click( () => {
-    
-    let input = $('#tasks').val();
-    let inputDate = $('#date').val();
-    
-    let newData = {
-        "task": input,
-        "dueDate": inputDate,
-        "action": "create"
-        
+        // Create HTML elements for each task.
+        let html = '<li><input type="checkbox"><span task-id="' + tasks[i].taskId + '">' + tasks[i].text + '</span><button>Edit</button><button class="delete-button">Delete</button></li>';
+
+        // Append newly created elements to the web page.
+        $('#todo-list ul').append(html);
     }
-        
-      // newTask is the handler for the server
-      $.post("https://app-todo-coderkarma.c9users.io/newTask", newData, (response, error) => {
-          
-         $("#message").text("Task Added!");
-          $("#message").fadeOut(2000, () => {
-              $(this).text("");
-              $(this).fadeIn(0);
-          });
-         
-         
-         $("#todo-list ul").empty();
-          
-          for (let i = 0; i < response.tasks.length; i++) {
-            console.log(response.tasks[i]);
-            
-            let html = '<li><input type="checkbox">' + response.tasks[i] + '<button>Edit</button><button class="delete-button">Delete</button></li>';
-            
-            $('#todo-list ul').append(html);
-            
-            }
-            
-            $(".delete-button").click(function (){
-                 console.log("Delete button pressed on no!!")
-              
-              }); 
-          
-      });
-    });
     
+    attachDeleteListeners();
+}
+
+// Attaches listeners to delete buttons.
+function attachDeleteListeners() {
+    $(".delete-button").click( function () {
+        // Remove parent of button clicked.
+        $(this).parent().remove();
+        
+        // Grab the text we want to delete
+        let currentTask = $(this).parent().children('span').text();
+        let currentTaskId =  $(this).parent().children('span').attr('task-id');
+        
+        let newData = {
+            task: currentTask,
+            taskId: currentTaskId,
+            dueDate: null,
+            action: "delete"
+        };
+        
+        console.log(newData);
+        
+        // Submit to the server the item we want to delete:
+        $.post("https://app-todo-coderkarma.c9users.io/deleteTask", newData, function (response) {
+            // -> update the List
+            updateTaskList(response);
+        });
+    });
+}
+
+$(document).ready(function() {
+    console.log("Ready");
+
+    // Create object to send to server. Should be empty.
+    let data = {};
+    
+    // Submit data to server and wait for response:
+    $.post("https://app-todo-coderkarma.c9users.io/fetchTasks", data, function(response) {
+        // Update the list view.
+        updateTaskList(response);
+    });
+
+    // When a person clicks on the Create New Task button:
+    $("#submitNewTask").click(function() {
+        
+        // Grab values from the input boxes.
+        let input = $("#task").val();
+        let inputDate = $("#date").val();
+        
+        
+        
+        // Store input data into an object.
+        let newData = {
+            "task": input,
+            "dueDate": inputDate,
+            "action": "create"
+        };
+        
+        // Send off object to server,  wait for response.
+        $.post("https://app-todo-coderkarma.c9users.io/newTask", newData, function(response) {
+            
+            // When responded:
+            // Put a message on the web page that the task has been added.
+            $("#message").text("Task Added!");
+            
+            // Fade out the message.
+            $("#message").fadeOut(2000, function () {
+               $(this).text("");
+               $(this).fadeIn(0);
+            });
+            
+            console.log(response);
+            // Update the list view.
+            updateTaskList(response);
+        });
+        
+
+    });
 });
--- main.js
+++ main.js
@@ -1,25 +1,23 @@
 let fs = require("fs");
 let bodyParser = require("body-parser");
 let express = require("express");
-let crypto = require("crypto");
-let mongoose = require("mongoose");
-
+const crypto = require("crypto");
+const mongoose = require('mongoose');
 
 let dbUrl = "mongodb://coderkarma:123456789k@ds119394.mlab.com:19394/todo-fall2018";
 
+mongoose.connect(dbUrl, function(error) {
 
-mongoose.connect(dbUrl, function(error){
-    if(error){
-        console.log("MongoDB connection failed");
-    } else {
-        console.log("MongoDB connectino successfull");
+    if (error) {
+        console.log("MongoDB connection failed!" + error);
     }
+    else {
+        console.log("MongoDB connection successfull!");
+    }
+
 });
 
-
 let Schema = mongoose.Schema;
-
-
 
 let app = express();
 
@@ -28,20 +26,20 @@ let http = require("http").Server(app);
 app.use('/', express.static("html_files/"));
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 http.listen(8080);
 
-function generateRandomString(length){
+function generateRandomString(length) {
     let hex = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM";
     let string = "";
-    
-    for ( let i = 0; i < length; i++){
-        string += hex.charAt(Math.floor(Math.random() * hex.length))
+
+    for (let i = 0; i < length; i++) {
+        string += hex.charAt(Math.floor(Math.random() * hex.length));
     }
+
     return string;
 }
-
 
 let Task = Schema({
     "text": String,
@@ -50,62 +48,92 @@ let Task = Schema({
     "taskId": String
 });
 
+let TodoModel = mongoose.model('tasks', Task);
 
-let TodoModel = mongoose.model("task", Task);
 
+function getTasksFromDB() {
 
+    TodoModel.find({}, function(error, result) {
+
+        if (error) {
+            console.log("Something went wrong!");
+            return -1;
+        }
+        else {
+            console.log(result);
+            return result;
+        }
+    });
+
+}
 
 console.log("Express server is now running on port 8080");
 
-let tasks = ["karma"];
+app.post("/fetchTasks", function(request, response) {
 
-app.post("/fetchTasks", (request, response) => {
-    
-    let data = {
-        "tasks": tasks
+    TodoModel.find({}, function(error, result) {
+
+        if (error) {
+            console.log("Something went wrong!");
+        }
+        else {
+            console.log(result);
+            response.send(result);
+        }
+    });
+
+});
+
+app.post("/newTask", function(request, response) {
+
+    let newTask = request.body;
+
+    let newTaskForDB = {
+        "text": newTask.task,
+        "status": "new",
+        "dueDate": newTask.dueDate,
+        "taskId": crypto.createHash("md5").update(newTask.task).digest("hex") + generateRandomString(8)
     };
-    
-    response.send(data);
-});
 
-
-app.post("/newTask", (request, response) => {
-     let newTask = request.body;
-     
-     let newTaskForDB = {
-         "text": newTask.task,
-         "status": "new",
-         "dueDate": newTask.dueDate,
-         "taskId": crypto.createHash("md5").update(newTask.task).digest("hex") + generateRandomString(8)
-     }
-    
     let taskToSubmit = TodoModel(newTaskForDB);
-    taskToSubmit.save();
-    
-    let data = {
-        "task": "" // mdb request , all task items
-    }
-    
-    response.send(data);
-    
+
+    taskToSubmit.save(function(error) {
+        if (error) {
+
+        }
+        else {
+            TodoModel.find({}, function(error, result) {
+
+                if (error) {
+                    console.log("Something went wrong!");
+                }
+                else {
+                    console.log(result);
+                    response.send(result);
+                }
+            });
+        }
+    });
 });
 
-app.post("/deleteTask", function (request, response) {
-    
+app.post("/deleteTask", function(request, response) {
+
     let recievedData = request.body;
-    let taskToDelete = recievedData.task;
-    
-    let index = tasks.indexOf(taskToDelete);
-    
-    if (index > -1) {
-        tasks.splice(index, 1);
-    }
-    
-    console.log(tasks);
-    
-    let data = {
-        "tasks": tasks
-    }
-    
-    response.send(data);
+    let taskToDelete = recievedData.taskId;
+
+     TodoModel.deleteOne({taskId: taskToDelete}, (error, result) => {
+        if(error){
+            console.log("something went wrong with delete");
+        } else {
+            TodoModel.find({}, (error, result) => {
+                if(error){
+                    console.log("could not find the list of tasks");
+                } else {
+                    response.send(result);
+                }
+            });
+         
+        }
+     });
+
 });
